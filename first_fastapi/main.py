d95b12ec3906dd08683bc3a84c8e9d604e888012
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-04-12 16:44:14 +0530

    query parameter

--- first_fastapi/main.py
+++ first_fastapi/main.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,22 @@ app = FastAPI()
 def index():
     # return "hello, i am pankaj"
     return {'data':{'name':'pankaj'}} # this is return like a json struture
+
+
+@app.get('/blog/public')
+def blog_public(limit=90,public:bool=True):
+    
+    if public:
+        return {'data':f"{limit} blogs from database is public"}
+    else:
+        return {'data':f"{limit} blog from database"}
+
+@app.get('/blog/{id}')
+def show(id:int): # id should be integer by default it string
+    return {'data':id}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished data'}
+
+

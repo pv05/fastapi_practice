dbb80c79e2cffdb70bca61a45973fa9f17de1632
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-04-12 12:32:14 +0530

    json to string change

--- first_fastapi/main.py
+++ first_fastapi/main.py
@@ -4,5 +4,5 @@ app = FastAPI()
 
 @app.get("/home") # that's called decorator
 def index():
-    # return "hello, i am pankaj"
-    return {'data':{'name':'pankaj'}} # this is return like a json struture
+    return "hello, i am pankaj"
+    # return {'data':{'name':'pankaj'}} # this is return like a json struture

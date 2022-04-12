a3a5560186347160c7f64092383e0f86b1dbb4d5
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-04-12 12:26:24 +0530

    first commit

---
+++ first_fastapi/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/home") # that's called decorator
+def index():
+    # return "hello, i am pankaj"
+    return {'data':{'name':'pankaj'}} # this is return like a json struture
